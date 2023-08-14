1bd6ab063552aa597cfdef6e66fd6ffff69612c3
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-08-14 04:13:51 +0300

    change Username Done

---
+++ src/pages/changeUsername.js
@@ -0,0 +1,69 @@
+import './styles/general.css'
+import './styles/changePassword.css'
+import checkAutherization from '../general/checkAuth';
+import Global from '../general/globalVar';
+import TopBar from '../components/topBar';
+import Input from '../components/input';
+import Button from '../components/button';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+function ChangeUsername() {
+    if (checkAutherization() !== 'Auth'){
+        window.location.href ='/login'
+    }
+    const lang = localStorage.getItem('lang') 
+    const [errorMessage,setErrorMessage] = useState(null)
+    const [correctMessage,setCurrectMessage] = useState(null)
+    const navigate = useNavigate()
+    const pageText = {
+        changeUsername: lang === 'en' ? "Change Username":"تغيير أسم المستخدم",
+        passwordShort:  lang === 'en' ? "Password you Entered is Short":"كلمة السر التي ادخلتها قصيره"
+    }
+    function submitChange(event){
+        const currentUsername = event.currentTarget.parentElement.parentElement.querySelector("#currentUsername").value
+        const newUsername = event.currentTarget.parentElement.parentElement.querySelector("#newUsername").value
+        const userPassword = event.currentTarget.parentElement.parentElement.querySelector("#userPassword").value
+        if (currentUsername.length < 3 || newUsername.length < 3){
+            setErrorMessage("Please Fill All the Input Fields")
+        }else if (userPassword.length < 5){
+            setErrorMessage(pageText['passwordShort'])
+
+        }else{
+            
+            axios.post(Global.BackendURL+"/editusername",{username:currentUsername,newUsername:newUsername,password:userPassword})
+            .then((res)=>{
+                const data = res.data
+                console.log(data)
+                localStorage.setItem("username",newUsername)
+                setErrorMessage(null)
+                setCurrectMessage("Username Changed Successfully")
+                navigate('/'+localStorage.getItem("role")+"/settings")
+            }).catch((err)=>{
+                
+                setErrorMessage(err.response.data)
+                console.log("Error!!\n",err)
+            })
+        }
+        console.log("currentUsername: "+currentUsername+" newUsername: "+newUsername+" userPassword: "+userPassword)
+    }
+    return (
+        <div id ={"ChangeUsername"} className="changeUsername column fullWidth">
+            <TopBar title={pageText["changeUsername"]}/>
+            <div className='pageContent'>
+                <div className='form'>
+                    <p className='red'>{errorMessage}</p>
+                    <p className='green'>{correctMessage}</p>
+                    <Input type={'text'} ID={'currentUsername'} label={"Current Username"}/>
+                    <Input type={'text'} ID={'newUsername'} label={"New Username"}/>
+                    <Input type={'password'} ID={'userPassword'} label={"Your Password"} showPassword={true}/>
+                    <br/>
+                    <Button text={"Submit"} onClickFunc={submitChange}/>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default ChangeUsername;
