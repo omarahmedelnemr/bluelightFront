91bc5e4acd2a2dd4071440fbb9bb494d76a7cc98
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-08-17 06:53:01 +0300

    add a simple View

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -3,35 +3,34 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
 function Logout() {
-    const navigate = useNavigate()
+    const lang = localStorage.getItem("lang")
+
+    const pageLang = {
+        logingout:  lang === 'en' ? "Logging out ...":"جار تسجيل الخروج ..."
+    } 
     const cookies= new Cookies()
     console.log("Request Sent")
     const keep = ['lang']
     const keepValues= {}
     for(var i =0;i<keep.length;i++){
-      keepValues[keep[i]] = localStorage.getItem(keep[i])
+        keepValues[keep[i]] = localStorage.getItem(keep[i])
     }
-    // localStorage.removeItem("img_dir")
-    // localStorage.removeItem("role")
-    // localStorage.removeItem("name")
-    // localStorage.removeItem("arname")
-    // localStorage.removeItem("classroom")
+
     localStorage.clear()
     for(var i =0;i<keep.length;i++){
-      localStorage.setItem(keep[i],keepValues[keep[i]])
+        localStorage.setItem(keep[i],keepValues[keep[i]])
     }
     cookies.remove("jwt")
     cookies.remove("role")
     cookies.remove("id")
     cookies.remove("CookiesState")
 
-//   navigate('/login')
     window.location.href = "./login"
-  return (
-    <div>
-      <h1>Logging out</h1>
-    </div>
-  );
+    return (
+        <div className='logout'>
+            <h1>{pageLang['logingout']}</h1>
+        </div>
+    );
 }
 
 export default Logout;
