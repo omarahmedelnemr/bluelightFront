4758d685ebaf306a71e6c36da4e016cd6e390572
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-08-17 06:46:54 +0300

    change Time format

--- src/general/formatTime.js
+++ src/general/formatTime.js
@@ -1,18 +1,23 @@
 function formatTime(timeString) {
-  if (timeString===null || timeString===undefined){
-    return 'none'
+  if (timeString === null || timeString === undefined) {
+    return 'none';
   }
   const date = new Date(timeString);
   
-  const month = date.getUTCMonth() + 1; // Months are zero-based
-  const day = date.getUTCDate();
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
   
   const period = hours >= 12 ? 'PM' : 'AM';
   const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
   
-  return `${month}/${day} ${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${period}`;
+  return `${month} ${day}, ${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${period}`;
 }
 
-export default formatTime
+export default formatTime;
