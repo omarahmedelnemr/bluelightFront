ff44ebabd5c81fbd6af47c2583870e41281c1628
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-08-22 20:07:18 +0300

    add labels

--- src/general/components/pieChart.js
+++ src/general/components/pieChart.js
@@ -1,6 +1,10 @@
 // src/components/PieChart.js
 import React from "react";
 import { Pie as PieJS } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+
+Chart.register(CategoryScale);
 
 function PieChart({ chartData,title }) {
   return (
