c54b9b94cd9496fac6900a37e01ec2748936e2b9
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-09-13 16:50:38 +0300

    fix for arabic

--- src/general/components/input.js
+++ src/general/components/input.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles/inputs.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Input({type,label,ID,showPassword = false}) {
+  const lang = localStorage.getItem("lang")
   var [icon,setIcon] = useState(showPassword ?  <FontAwesomeIcon icon="fa-solid fa-eye" onClick={showPass}/>:null);  
   function showPass(event){
     console.log(event.currentTarget.parentElement.querySelector('input').type)
@@ -16,7 +17,7 @@ function Input({type,label,ID,showPassword = false}) {
     }
   }
   return (
-    <div className="col-3 input-effect">
+    <div className={`col-3 input-effect ${lang}`}>
         <input className="effect-22" type={type} id = {ID} name ={label} placeholder=" "/>
         <label>{label}</label>
         <span className="focus-bg"></span>
