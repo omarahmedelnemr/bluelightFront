d9aca54f600d72e23a72d1a5766e734cdb4f0a68
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-12-18 01:23:44 +0200

    remove the redirect

--- src/publicFunctions/axiosAuth.js
+++ src/publicFunctions/axiosAuth.js
@@ -10,21 +10,21 @@ axios.interceptors.request.use(function (config) {
     return config;
 });
 
-axios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      if (error.response.status === 401) {
-        // Redirect to the login page when a 401 error is encountered
-        if (window.location.pathname !== '/login'){
-            window.location.href = '/login'
-        }else{
-            console.log("Already on Login")
-        }
-      }
-      return Promise.reject(error);
-    }
-  );
+// axios.interceptors.response.use(
+//     (response) => {
+//       return response;
+//     },
+//     (error) => {
+//       if (error.response.status === 401) {
+//         // Redirect to the login page when a 401 error is encountered
+//         if (window.location.pathname !== '/login'){
+//             window.location.href = '/login'
+//         }else{
+//             console.log("Already on Login")
+//         }
+//       }
+//       return Promise.reject(error);
+//     }
+//   );
 
 export default axios
